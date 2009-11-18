78f22dd928ac722cf55b3c1fbd661eee0def1cc5
Daisuke Murase <typester@cpan.org>
2009-11-18 19:16:55 +0900

    initial import

---
+++ .nim
@@ -0,0 +1,43 @@
+log_level: debug
+
+data_dir: ./data
+site_dir: ./public
+
+plugins:
+  - module: Entry::File
+  - module: Entry::Clmemo
+    config:
+      file: './clmemo.txt'
+      limit: 30
+
+  - module: Meta
+  - module: Markdown
+
+  - module: Render::Entry
+    rule:
+      expression: $entry->path !~ /^clmemo/;
+
+  - module: Render::Entry
+    rule:
+      expression: $entry->path =~ /^clmemo/;
+    config:
+      path: memo/{year}/{month}/{day}
+      filename: '{filename}/index.html'
+
+  - module: AutoIndex
+    config:
+      filter: $entry->path !~ /^clmemo/;
+
+  - module: AutoIndex
+    config:
+      filter: $entry->path =~ /^clmemo/;
+      path: memo/{year}/{month}/{day}
+
+  - module: AutoIndex
+    config:
+      filter: $entry->path =~ /^clmemo/;
+      path: memo
+
+  - module: Paginate
+    config:
+      entries_per_page: 10
