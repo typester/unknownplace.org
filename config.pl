9e07ef663c8b6c605c850eec983b3b43c5605caf
Daisuke Murase <typester@cpan.org>
2013-06-11 10:20:42 +0900

    conf env tweaks for daemontools

--- config.pl
+++ config.pl
@@ -6,7 +6,10 @@ use File::Basename;
 our $blog_title    = 'unknownplace.org';
 our $blog_encoding = 'utf-8';
 
-our $datadir          = "$ENV{HOME}/Dropbox/blosxom";
+our $datadir = $ENV{HOME} =~ /typester/
+    ? "$ENV{HOME}/Dropbox/blosxom"
+    : "/home/typester/Dropbox/blosxom";
+
 our $plugin_dir       = dirname(__FILE__) . "/plugins";
 our $plugin_state_dir = dirname(__FILE__) . "/plugins/state";
 
