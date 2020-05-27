872b73d22156e8dc196fa9c05552c69672990485
Daisuke Murase <typester@cpan.org>
2020-05-27 05:28:16 -0700

    organize imports

--- components/blog.tsx
+++ components/blog.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import Link from 'next/link';
-import moment from 'moment';
+import React from 'react';
 import { BlogDetail } from '../entities/Blog';
 
 interface BlogEntryViewProps {
